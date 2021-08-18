3c9294b0e0839ce009353290295a6dc4b1abcaab
amater95 <73442543+amater95@users.noreply.github.com>
2021-08-18 21:15:50 +0300

    Задание 5

--- script.js
+++ script.js
@@ -1,89 +1,105 @@
-class ProductList {
-    constructor(container = '.products') {
-        this.container = container;
-        this.goods = [];
-        this.allProducts = []; //массив товаров с версткой
-        this._fetchProducts();
-        this.render(); //вывод товаров на страницу
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        isVisibleCart: false,
+        searchLine: '',
+        catalogUrl: '/catalogData.json',
+        products: [],
+        cartItems: [],
+        isCartFull: false,
+        cartProductsCount: 0,
+        cartProductsSum: 0
+
+    },
+    computed: {
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result) {
+                        let find = this.cartItems.find(el => el.id_product === product.id_product);
+                        this.cartProductsCount++;
+                        this.cartProductsSum += product.price;
+                        if (find) {
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({
+                                quantity: 1
+                            }, product);
+                            this.cartItems.push(prod);
+                        }
+                        this.isCartFull = !!this.cartItems.length;
+
+                        console.log(this.cartProductsCount, this.cartProductsSum);
+                    } else {
+                        console.log('Error!');
+                    }
+                })
+        },
+        removeProduct(product) {
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if (data.result) {
+                        let id = +product.id_product;
+                        let find = this.cartItems.find(el => el.id_product === id);
+                        this.cartProductsCount--;
+                        this.cartProductsSum -= product.price;
+                        if (find.quantity > 1) {
+                            find.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(find), 1);
+                        }
+                        this.isCartFull = !!this.cartItems.length;
+                    } else {
+                        console.log('Error!');
+                    }
+                })
+        },
+        FilterGoods(value) {
+            const regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+            this.products.forEach(el => {
+                const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+                if (this.filtered.includes(el)) {
+                    block.classList.remove('invisible')
+                } else {
+                    block.classList.add('invisible')
+                }
+            })
+        },
+        init() {
+            document.querySelector('.search-form').addEventListener('submit', e => {
+                e.preventDefault();
+            });
+            if (this.cartItems.length) {
+                this.isCartFull = true;
+            }
+        },
+    },
+    mounted() {
+        this.init();
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            })
     }
-    _fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'мышь', price: 200 },
-            { id: 2, title: 'клавиатура', price: 500 },
-            { id: 3, title: 'монитор', price: 15000 },
-            { id: 4, title: 'колонки', price: 3500 },
-            { id: 5, title: 'сетевая плата', price: 750 },
-            { id: 6, title: 'видеокарта', price: 100000 },
-            { id: 7, title: 'блок питания', price: 5000 },
-            { id: 8, title: 'куллер', price: 1000 },
-        ];
-    }
-    render() {
-        const block = document.querySelector(this.container);
-        for (let product of this.goods) {
-            const item = new ProductItem(product);
-            this.allProducts.push(item);
-            block.insertAdjacentHTML("beforeend", item.render());
-        }
-    }
-
-    totalSum() {
-        let sum = 0;
-        this.goods.forEach((product) => {
-            sum += product.price;
-        });
-        document.querySelector('.sum').innerHTML = `Сумма заказа: ${sum} рублей`;
-    }
-}
-
-
-class ProductItem {
-    constructor(product) {
-        this.title = product.title;
-        this.id = product.id;
-        this.price = product.price;
-    }
-    render() {
-        return `<div class="product-item">
-                <h3>${this.title}</h3>
-                <p>${this.price}руб</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-    }
-}
-
-class CartList {
-    constructor() {
-
-    }
-    // метод для очистки корзины
-    clearAll() {
-
-    }
-    //метод подсчета суммы купленных товаров
-    totalSum() {
-
-    }
-    //метод оплаты товаров
-    pay() {
-
-    }
-}
-
-class CartItem {
-    constructor() {
-
-    }
-    //удаляет один элемент из корзины
-    deleteItem() {
-
-    }
-    //добавляет один элемент, который уже есть в корзине
-    sumItem() {
-
-    }
-
-}
-
-let list = new ProductList();
-list.totalSum();
+});
