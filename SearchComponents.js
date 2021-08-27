0013eb45b34ae79cf1c48af2b875087abea7a754
amater95 <73442543+amater95@users.noreply.github.com>
2021-08-28 00:43:46 +0300

    Update SearchComponents.js

--- SearchComponents.js
+++ SearchComponents.js
@@ -1,8 +1,10 @@
-Vue.component('search-form', {
-    data: {
-        userSearch: '',
+Vue.component('filter-el', {
+    data() {
+        return {
+            userSearch: ''
+        }
     },
-    template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
+    template: `<form action="#" method="post" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
                  <input type="text" class="search-field" v-model="userSearch">
                  <button class="btn-search" type="submit">
                      <i class="fas fa-search"></i>
