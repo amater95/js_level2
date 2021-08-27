6f042e40d78e54ae08ffcc2b357f0a099936404c
amater95 <73442543+amater95@users.noreply.github.com>
2021-08-28 00:43:23 +0300

    Update ProductComponent.js

--- ProductComponent.js
+++ ProductComponent.js
@@ -1,47 +1,41 @@
 Vue.component('products', {
     data() {
         return {
-            catalogUrl: '/catalogData.json',
+            catalogUrl: `/catalogData.json`,
             products: [],
             filtered: [],
         }
     },
     methods: {
-        filter(userSearch) {
-            let regexp = new RegExp(userSearch, 'i');
+        filter(value) {
+            let regexp = new RegExp(value, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
     mounted() {
-        this.$parent.getJson(`${API + this.catalogUrl}`)
+        this.$parent.getJson(`/api/products`)
             .then(data => {
                 for (let el of data) {
                     this.products.push(el);
                     this.filtered.push(el);
                 }
             });
-        this.$parent.getJson(`getProducts.json`)
-            .then(data => {
-                for (let el of data) {
-                    this.products.push(el);
-                    this.filtered.push(el);
-                }
-            })
     },
     template: `<div class="products">
-                <product v-for="item of filtered" :key="item.id_product" :product="item">
-                </product>
-            </div>`
+            <product 
+            v-for="product of filtered" 
+            :key="product.id_product"
+            :product="product"
+        </div>`
 });
 Vue.component('product', {
-    props: ['product'],
-    template: `
-            <div class="product-item">
+    props: ['product', 'img'],
+    template: `<div class="product-item standart p-3" >
                 <div class="desc">
-                    <h3>{{product.product_name}}</h3>
-                    <p>{{product.price}} руб</p>
-                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>
+                    <h3 class="price-name text-white pt-3">{{ product.product_name }}</h3>
+                    <div class="price d-flex justify-content-center align-items-end"><p>{{ product.price }}$</p></div>
+                    <div class="price-info text-white">{{ product.price_info }}</div>
+                    <button class="buy-btn ml-3" @click="$root.$refs.cart.addProduct(product)">Купить</button>
                 </div>
-            </div>
-    `
+            </div>`
 })
